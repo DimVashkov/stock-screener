26240ae2a5fef4272e2631f31468cf286b98681f
DimVashkov <dimitarvashkov@outlook.com>
2021-12-23 11:46:39 +0200

    added func to add tickers

--- src/app/modules/home/components/stock-list/stock-list.component.ts
+++ src/app/modules/home/components/stock-list/stock-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-stock-list',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-list.component.scss']
 })
 export class StockListComponent implements OnInit {
+  @ViewChild('stockInput') stockInput: ElementRef | undefined;
+  stocks: string[] = ['AAPL', 'TSLA', 'MSFT', 'NVDA', 'IBM'];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  addStockTicker(ticker: string) {
+    ticker = ticker.toUpperCase();
+    if (ticker != '' && !this.stocks.includes(ticker)) {
+      this.stocks.push(ticker);
+      this.clearStockInput();
+    }
+  }
 
+  clearStockInput(): void {
+    if (this.stockInput != undefined) {
+      this.stockInput.nativeElement.value = '';
+    }
+  }
 }
