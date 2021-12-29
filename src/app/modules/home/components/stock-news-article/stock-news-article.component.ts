7afda764105ea76b7a1abebcff5549ec1f906e56
DimVashkov <dimitarvashkov@outlook.com>
2021-12-29 20:52:56 +0200

    change component selector to get around css problem

--- src/app/modules/home/components/stock-news-article/stock-news-article.component.ts
+++ src/app/modules/home/components/stock-news-article/stock-news-article.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NewsArticle } from 'src/app/shared/interfaces/news-article';
 
 @Component({
-  selector: 'app-stock-news-article',
+  // Otherwise I have <app-stock-news-article> and that messes up my CSS. This is a bad practice, ask for alternatives.
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: '[app-stock-news-article]',
   templateUrl: './stock-news-article.component.html',
   styleUrls: ['./stock-news-article.component.scss']
 })
